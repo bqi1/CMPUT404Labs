4920d7faafbc8be475ec0ae17d5b7f19c4c52634
Brian Qi <brian.qi138@gmail.com>
2021-01-11 18:16:58 -0700

    Modified Script

--- Script.py
+++ Script.py
@@ -2,4 +2,6 @@ import requests
 
 # print(requests.__version__)
 
-r = requests.get('https://www.google.com')
+# r = requests.get('https://www.google.com')
+r = requests.get('https://raw.githubusercontent.com/bqi1/CMPUT404Labs/master/Script.py')
+print(r.text)
